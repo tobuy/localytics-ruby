d5cf3323df5aeb34d4e4d17d7735e48b3cb4c0b1
Matthew Hively <matthewhively@viz.com>
2020-08-04 15:39:21 -0700

    Adjust spacing and use () for methods

--- lib/localytics/event.rb
+++ lib/localytics/event.rb
@@ -1,4 +1,5 @@
 module Localytics
+
   class Event
     class << self
       attr_accessor :app_id
@@ -10,8 +11,8 @@ module Localytics
     #                     Must be integer, e.g. use 299 for $2.99.
     def self.send(app_id, customer_id, event_name, event_attributes=nil, ltv_change=nil, api_key=nil, api_secret=nil)
       raise Error.new('No APP id provided') unless app_id ||= self.app_id
-      raise Error.new('No customer_id provided') if customer_id.nil?
-      raise Error.new('No event_name provided') if event_name.nil? || event_name.empty?
+      raise Error.new('No customer_id provided')  if  customer_id.nil?
+      raise Error.new('No event_name provided')   if  event_name.nil? || event_name.empty?
 
       params = {
         schema_url:  "https://localytics-files.s3.amazonaws.com/schemas/eventsApi/v0.json",
@@ -21,13 +22,14 @@ module Localytics
         event_time:  (Time.now.to_f * 1000).to_i,
         uuid:        SecureRandom.uuid
       }
-      params[:attributes] = event_attributes if event_attributes && !event_attributes.empty?
-      params[:ltv_change] = ltv_change if ltv_change
+      params[:attributes] = event_attributes if  event_attributes && !event_attributes.empty?
+      params[:ltv_change] = ltv_change       if  ltv_change
 
       Localytics.request api_base, :post, url, api_key, api_secret, params
     end
 
     private
+
     def self.api_base
       "https://analytics.localytics.com/events/v0"
     end
@@ -36,4 +38,5 @@ module Localytics
       "/uploads"
     end
   end
+
 end
