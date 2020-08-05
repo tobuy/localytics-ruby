bf5f2d4270fde73c49b8bd11b5cd3da14b4490bf
Matthew Hively <matthewhively@viz.com>
2020-08-05 06:53:03 -0700

    Add support for invoking localytics events API (#16)
    * Add support for invoking localytics beta events API.
    
    * can't use present? or blank?
    
    as they are defined in rails env. gem needs to work with out rails.
    
    * update version number
    
    * add basic test of Event.send
    
    Remove calls to present
    
    * (comment fix)
    
    * Adjust spacing and use () for methods
    
    * gitignore skip vim files
    
    * Update to v1 (no longer beta)
    
    * Consolidated gemspec file defintions, ruby version to 2.5.5
    
    Co-authored-by: Siddharth Patel <sidcpatel@users.noreply.github.com>
    Co-authored-by: Andrew Zenk <andrew.zenk@viz.com>
    Co-authored-by: Sid Patel <sid.c.patel@gmail.com>

---
+++ lib/localytics/event.rb
@@ -0,0 +1,42 @@
+module Localytics
+
+  class Event
+    class << self
+      attr_accessor :app_id
+    end
+
+    # @param event_attributes = Optional hash of up to 50 key/value attribute pairs.
+    #                           Values must be formatted as strings.
+    # @param ltv_change = Optional int representing incremental change in user's lifetime value.
+    #                     Must be integer, e.g. use 299 for $2.99.
+    def self.send(app_id, customer_id, event_name, event_attributes=nil, ltv_change=nil, api_key=nil, api_secret=nil)
+      raise Error.new('No APP id provided') unless app_id ||= self.app_id
+      raise Error.new('No customer_id provided')  if  customer_id.nil?
+      raise Error.new('No event_name provided')   if  event_name.nil? || event_name.empty?
+
+      params = {
+        schema_url:  "https://localytics-files.s3.amazonaws.com/schemas/eventsApi/v1.json",
+        app_uuid:    app_id,
+        customer_id: customer_id.to_s,
+        event_name:  event_name,
+        event_time:  (Time.now.to_f * 1000).to_i,
+        uuid:        SecureRandom.uuid
+      }
+      params[:attributes] = event_attributes if  event_attributes && !event_attributes.empty?
+      params[:ltv_change] = ltv_change       if  ltv_change
+
+      Localytics.request api_base, :post, url, api_key, api_secret, params
+    end
+
+    private
+
+    def self.api_base
+      "https://analytics.localytics.com/events/v1"
+    end
+
+    def self.url
+      "/uploads"
+    end
+  end
+
+end
--- localytics-ruby.gemspec
+++ localytics-ruby.gemspec
@@ -2,30 +2,18 @@
 
 Gem::Specification.new do |s|
   s.name              = 'localytics-ruby'
-  s.version           = '0.0.5'
+  s.version           = '0.0.7'
   s.summary           = 'Ruby wrapper for Localytics API'
   s.description       = 'API to interact with Localytics https://localytics.com/'
   s.authors           = ['Tobuy development team', 'Marcos Chicote']
   s.email             = ['support@tob.uy']
   s.homepage          = 'https://github.com/tobuy/localytics-ruby'
-  s.files             = []
-
   s.license           = 'MIT'
+
+  s.files      = Dir["lib/**/*"] + ["LICENSE", "README.md"]
+  s.test_files = Dir["test/*"]
+
   s.add_dependency('rest-client', '~> 2.0')
   s.add_development_dependency('cutest', '~> 1.2')
   s.add_development_dependency('mocha', '~> 1.1')
-
-  s.files = %w{
-    lib/localytics.rb
-    lib/localytics-ruby.rb
-    lib/localytics/profile.rb
-    lib/localytics/push.rb
-    lib/localytics/app.rb
-  }
-
-  s.test_files = %w{
-    test/localytics_test.rb
-    test/test_helper.rb
-  }
-
 end
